d8a2a11148da336b401e9557d739cfcafdae7b0f
Ian Mobbs <ianmobbs@gmail.com>
2021-05-20 16:36:11 -0700

    Added first test file

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    moduleNameMapper: {
+        "@app/(.*)": "<rootDir>/app/$1"
+    },
+    preset: "ts-jest",
+    testEnvironment: "node"
+}
---
+++ test/services/url-service.test.ts
@@ -0,0 +1,38 @@
+import User from "../../app/model/entities/User";
+
+const MOCK_URL_MANAGER = {
+    init: jest.fn(),
+    getAllURLsForUser: jest.fn()
+}
+const mockUser: User = {
+    id: '12345',
+    email: 'ianmobbs@gmail.com',
+    passwordHash: '12345'
+}
+const MOCK_URL: URL = {
+    clicks: [],
+    id: '12345',
+    user: mockUser,
+    shortUrlId: 'abcdef',
+    longURL: 'google.com',
+    expiration: 12345
+};
+
+jest.mock('@app/managers/url-manager.ts', () => {
+    return jest.fn().mockImplementation(() => MOCK_URL_MANAGER);
+});
+
+import URLService from "../../app/services/url-service";
+import URL from "../../app/model/entities/URL";
+const urlService = new URLService();
+
+describe('url-service.ts unit tests', () => {
+    describe('getAllURLsForUser', () => {
+        it('should return an array of URLs', async () => {
+            MOCK_URL_MANAGER.getAllURLsForUser.mockReturnValue([MOCK_URL])
+            const urls = await urlService.getAllURLsForUser(mockUser)
+            expect(MOCK_URL_MANAGER.getAllURLsForUser).toBeCalledWith(mockUser)
+            expect(urls).toEqual([MOCK_URL]);
+        })
+    })
+})
